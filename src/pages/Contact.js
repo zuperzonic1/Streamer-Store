fb24e3edbe926272c95b1bc5c4e71f5923e8a80e
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2023-11-22 00:20:34 -0500

    profile changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,29 +1,58 @@
+import { useNavigate } from 'react-router-dom';
 
-import {useNavigate} from 'react-router-dom';
-import {Link} from 'react-router-dom';
-// import React, {useState} from "react";
+function Profile(props) {
+    const navigate = useNavigate();
 
-
-    function Profile(props){
-
-        const navigate = useNavigate();
-    
-        function goToContactConfirmation(e){
-            e.preventDefault();
-            console.log("Imagine the future");
-            navigate('profileedit');
-        }
-
-
-        
-        return (
-            <form onSubmit={goToContactConfirmation}>
-                <h1>Profile Page</h1>
-                <h2>Name</h2>
-                <h2>Last name</h2>
-
-                <input className='contact-button' type="submit" value="Edit profile" />
-            </form>
-        )
+    function goToProfileEdit(e) {
+        e.preventDefault();
+        console.log("Navigating to profile edit");
+        navigate('profileedit');
     }
+
+    return (
+        <form onSubmit={goToProfileEdit} className="profile-form">
+            <h1 className="profile-title">Profile Page</h1>
+
+            <div className="profile-details">
+                <div className="profile-detail-item">
+                    <h2 className="detail-title">Name:</h2>
+                    <p className="detail-info">John</p>
+                </div>
+
+                <div className="profile-detail-item">
+                    <h2 className="detail-title">Last Name:</h2>
+                    <p className="detail-info">Doe</p>
+                </div>
+
+                <div className="profile-detail-item">
+                    <h2 className="detail-title">Email:</h2>
+                    <p className="detail-info">johndoe@example.com</p>
+                </div>
+
+                <div className="profile-detail-item">
+                    <h2 className="detail-title">Phone Number:</h2>
+                    <p className="detail-info">+1234567890</p>
+                </div>
+
+            </div>
+
+            <input className="edit-profile-button" type="submit" value="Edit profile" />
+
+            <div className="past-orders-section">
+                <h2 className="orders-title">Past Orders</h2>
+                <div className="order-item">
+                    <p className="order-number">Order #12345</p>
+                    <p className="order-date">Date: 01/01/2023</p>
+                    <p className="order-status">Status: Delivered</p>
+                </div>
+                <div className="order-item">
+                    <p className="order-number">Order #12345</p>
+                    <p className="order-date">Date: 01/01/2023</p>
+                    <p className="order-status">Status: Delivered</p>
+                </div>            
+                </div>
+        </form>
+    )
+}
+
 export default Profile;
--- src/pages/ContactConfirmation.js
+++ src/pages/ContactConfirmation.js
@@ -1,35 +1,31 @@
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-
-function ProfileEdit(props){
-
+function ProfileEdit(props) {
     const navigate = useNavigate();
 
-    function goToThanks(e){
+    function goToThanks(e) {
         e.preventDefault();
-        console.log("takes us to homepage");
-        navigate('/imagine');
+        navigate('/categories');
     }
+
     return (
-        <form className='cont' onSubmit={goToThanks}>
-              <h1>EDIT PROFILE</h1>
-                <section className='contact-section'>
-                    <label className='contact-label'>First name:
-                        <input className='contact-input' type="text" name='firstName'  onChange={props.handleFirstNameChange} placeholder='First name ...'/>
-                    </label>
-                    <label className='contact-label'>Last name:
-                        <input className='contact-input' type="text" name='lastName'   onChange={props.handleLastNameChange} placeholder='Last name ...'/>
-                    </label>
-                   
-                    <label className='contact-label'>Age:
-                    <input className='contact-input' type="text" name='age'  onChange={props.handleAgeChange} placeholder='Age ...'/>
-                    </label>
-                    <label className='contact-label'>Bio:
-                    {/* how to make text box bigger */}
-                        <input className='contact-input' type="text" name='message'  onChange={props.handleMessageChange} placeholder='Message ...'/>
-                    </label>
-                    <input className='contact-button' type="submit" value="Submit" />
-                </section>
+        <form className='profile-edit-form' onSubmit={goToThanks}>
+            <h1 className='profile-edit-title'>EDIT PROFILE</h1>
+            <section className='profile-edit-section'>
+                <label className='profile-edit-label'>First name:
+                    <input className='profile-edit-input' type="text" name='firstName' onChange={props.handleFirstNameChange} placeholder='First name ...'/>
+                </label>
+                <label className='profile-edit-label'>Last name:
+                    <input className='profile-edit-input' type="text" name='lastName' onChange={props.handleLastNameChange} placeholder='Last name ...'/>
+                </label>
+                <label className='profile-edit-label'>Age:
+                    <input className='profile-edit-input' type="text" name='age' onChange={props.handleAgeChange} placeholder='Age ...'/>
+                </label>
+                <label className='profile-edit-label'>Bio:
+                    <textarea className='profile-edit-textarea' name='message' onChange={props.handleMessageChange} placeholder='Bio ...'></textarea>
+                </label>
+                <input className='profile-edit-submit' type="submit" value="Submit" />
+            </section>
         </form>
     )
 }
